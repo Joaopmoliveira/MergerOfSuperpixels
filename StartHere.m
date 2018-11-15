bd8e1a8fef4ee277d9a49162c298bd657c0ef67a
João Oliveira <44142506+Joaopmoliveira@users.noreply.github.com>
2018-11-15 22:21:18 +0000

    Add files via upload

--- StartHere.m
+++ StartHere.m
@@ -4,15 +4,15 @@ clearvars
 addpath([pwd,'\GUIuser'])
 %Here the algorithm asks the user for the image he wants so select 
 %and what are the parameters for the SLIC method
-[B,L,N,idx,BW,handles]=preprocessing();
+[B,L,N,BW,handles]=preprocessing();
 %Here the properties of the Superpixels are processed
-[ColorVector,adj] = sPproperties(B,N,idx,L);
+handles = sPproperties(B,N,L,handles);
 %Here the superpixels that will be connected are identified
-[MergeMatrix,handles] = mergeSP(adj,ColorVector,0.04,N,B,BW,handles);
+handles = mergeSP(N,B,BW,handles);
 %The graph resulting from the connected superpixels is identified and 
 % the final resulting hyperpixels (the previous superpixels connected)
 % are created
-[idxfinal,Lf] = graphMerge(MergeMatrix,L,idx);
+[handles,Lf] = graphMerge(L,handles);
 %the final results are shown to the user
-PlotFinalResults(handles,B,BW,idxfinal,idx,Lf)
+PlotFinalResults(handles,B,BW,Lf)
 end
